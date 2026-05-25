4009d45d5d2733d05879ed99c7a06ddabfb9d74d
shiv-rana001 <shiv242001rana@gmail.com>
2026-05-25 21:57:33 +0530

    ..

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Link } from "react-router";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Link to="/">
+        <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+      </Link>
+      <Link to="/upload" className="primary-button w-fit">
+        Upload Resume
+      </Link>
+    </div>
+  );
+};
+export default Navbar;
